a2cddd03df1471becc132340ef3fadfae95ceba5
dpaschal-lf <daniel.paschal@learningfuze.com>
2016-06-07 13:40:05 -0700

    modifying phpmailer

---
+++ php_oauth_facebook/facebook_info.php
@@ -0,0 +1,6 @@
+<?php
+define('FACEBOOK_APP_ID','1691779651071505');
+define('FACEBOOK_SECRET','6723bbfe1cae8a3c6fe38b63060439a1');
+define('SERVER_LANDING','https://danielpaschal.com/fb-callback.php');
+define('FACEBOOK_GRAPH_VERSION','v2.6');
+?>
