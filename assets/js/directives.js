278928657bed1bf44ad84dfd494e22d019dc2430
Daniel Paschal <daniel.paschal@learningfuze.com>
2016-11-06 09:45:43 -0800

    updated to 11.16

--- assets/js/directives.js
+++ assets/js/directives.js
@@ -32,7 +32,7 @@ app.directive("instructions", function(){
 
         },
         link: function(scope, element){
-            scope.repoName = "prototypes_C10";
+            scope.repoName = "prototypes_C11.16";
             scope.map = {17: false, 18: false, 90: false};
 
             $('body').on("keydown", function(e){
