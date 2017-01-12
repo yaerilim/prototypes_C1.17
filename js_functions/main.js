7c3852027b186ed032a987e12661788ea6d61053
Yaeri Lim <yaerilim@gmail.com>
2017-01-11 16:31:44 -0800

    js functions - Yaeri Lim

---
+++ js_functions/main.js
@@ -0,0 +1,14 @@
+function myMessage(){
+    console.log("This is my first function!");
+}
+function add(x, y){
+    console.log(x + y);
+}
+function add2(x, y){
+    var total = x + y;
+    return total;
+}
+var add2result = add2(10, 36);
+function cardFlip(element){
+    $(element).hide();
+}
