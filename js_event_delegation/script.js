a9e5e5187662b0a43fd25609b556ffdedd3efa72
Yaeri Lim <yaerilim@gmail.com>
2017-01-20 00:32:55 -0800

    js event delegation - Yaeri Lim

--- js_event_delegation/script.js
+++ js_event_delegation/script.js
@@ -1,10 +1,29 @@
 // Answer the questions here:
 
 // - What do you think is going to happen?
-
+//nothing
 // - What happened?
-
+//nothing
 // - Why?
-
+//Becuase the 5th button was made after console.log($(this).text());???
 //========== Write your code below ===========//
 
+$(document).ready(function() {
+    $('#list button').on('click', function () {
+        console.log($(this).text());
+    });
+
+    $('#list').append("<li><button style='margin-top: 10px'>Delegated Button#5 Handler</button></li>");
+
+    $('#list button').on('click', function () {
+        console.log($(this).text());
+    });
+
+    $('#list').append("<li><button id='google'>Open Google!</button></li>");
+
+    $('#list').on('click', '#google', function() {
+        if($(event.target).attr('id') !== undefined){
+            window.open("http://www.google.com", '_blank');
+        }
+    });
+});
