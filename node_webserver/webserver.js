d8784146798e8543e51803176159069e725a073e
Yaeri Lim <yaerilim@gmail.com>
2017-02-19 19:06:31 -0800

    Create your basic web server based on your notes

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,1 +1,9 @@
-
+var http = require('http');
+var server = http.createServer(function handleRequest(request, response){
+    console.log('handling request');
+    response.write('initializing request response\n');
+    response.end('It Works!! Path Hit: ' + request.url);
+});
+server.listen(8888, function(){
+    console.log('Server Listening on: http://localhost:8888');
+});
