4ba102102724a80c07b5e8ee15454bbd625e27aa
Yaeri Lim <yaerilim@gmail.com>
2017-02-20 12:25:56 -0800

    v.5 fixed

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,14 +1,19 @@
 var http = require('http');
-var fs = require(fs);
-var server = http.createServer(function handleRequest(request, response){
-    console.log('handling request');
-    response.write('initializing request response\n');
-    response.end('It Works!! Path Hit: ' + request.url);
-});
-server.listen(8888, function(){
-    console.log('Server Listening on: http://localhost:8888');
-});
-fs.readFile('index.html', function (err, data) {
-    if (err) throw err;
-    console.log(data);
+var fs = require('fs');
+//var server =
+
+fs.readFile('./index.html', function (err, data) {
+    if (err) {
+        throw err;
+        console.log(data);
+    }
+    http.createServer(function (request, response){
+        console.log('handling request');
+        response.writeHeader(200,{"content-type" : "text/html"});
+        response.write(data);
+        // response.write('initializing request response\n');
+        response.end('It Works!! Path Hit: ' + request.url);
+    }).listen(8888, function() {
+        console.log('Server Listening');
+    });
 });
